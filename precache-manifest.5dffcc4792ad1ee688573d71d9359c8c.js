251eb93e11dcb22e2a84023625df7a442f1ad5a9
nshganesh <nshganesh@gmail.com>
2019-12-29 12:23:18 +0530

    Updates

---
+++ precache-manifest.5dffcc4792ad1ee688573d71d9359c8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7741f08d99b3b1002b9040d197984824",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "99ff78e54b9a832409b7",
+    "url": "/resume/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a0b836e090910163b943",
+    "url": "/resume/static/js/2.0ec32d5c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/resume/static/js/2.0ec32d5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99ff78e54b9a832409b7",
+    "url": "/resume/static/js/main.5f7b59c5.chunk.js"
+  },
+  {
+    "revision": "754cb4722296976aff30",
+    "url": "/resume/static/js/runtime-main.bef2460a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
